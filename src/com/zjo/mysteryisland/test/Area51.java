229de8bcb5c42d1b369e0bf6495cdf08b12a939d
zachowens <zachowens@zachs-mbp.attlocal.net>
2017-08-29 14:02:34 -0700

    Add test package and Area51 class.

---
+++ src/com/zjo/mysteryisland/test/Area51.java
@@ -0,0 +1,13 @@
+package com.zjo.mysteryisland.test;
+
+public class Area51 {
+	
+
+	public static void main(String[] args) {
+		
+		int testInt;
+		testInt = (int) 27.6;
+		
+		System.out.println(testInt);
+	}
+}
