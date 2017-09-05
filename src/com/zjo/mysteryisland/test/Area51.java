3a653f55491675114ee21550490aabf880472fc4
zachowens <zachowens@zachs-mbp.attlocal.net>
2017-09-05 12:19:30 -0700

    Add DecimalFormat test.

--- src/com/zjo/mysteryisland/test/Area51.java
+++ src/com/zjo/mysteryisland/test/Area51.java
@@ -1,12 +1,16 @@
 package com.zjo.mysteryisland.test;
 
+import java.text.DecimalFormat;
+
 public class Area51 {
 	
 	public static void main(String[] args) {
 		
 		int testInt;
 		testInt = (int) 27.6;
-		
 		System.out.println(testInt);
+		
+		DecimalFormat dfQuestion = new DecimalFormat("#0.##E0");
+		System.out.println(dfQuestion.format(12.7896987));
 	}
 }
